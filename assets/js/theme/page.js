5c13b340e37ba250886eaa41c4c66da84f108ce3
Pete Arevalo Admin <pete.arevalo-admin@sg.local>
2017-07-24 16:20:45 -0500

    started vid gallery and standard product page

--- assets/js/theme/page.js
+++ assets/js/theme/page.js
@@ -1,3 +1,10 @@
 import PageManager from './page-manager';
 
 export default class Page extends PageManager {}
+
+    //Efficency for dynamically loading youtube thumbnails
+    $(".youtube-video-gallery").each(function () {
+
+        $(this).css('background-image', 'url(//i.ytimg.com/vi/' + this.id + '/maxresdefault.jpg)');
+
+    });
